ef58b47fc2f4259ce1781f1edb6a73f16ac0a56f
Eliott <eliott.benoit4@gmail.com>
2019-04-19 13:00:31 +0200

    switch from ivona api to gTTS

--- README.txt
+++ README.txt
@@ -1,4 +1,3 @@
 Tweet2Speech streams over tweets from a specified target profile.
-For each new tweet, T2S get the oral speech from the text thanks to Ivona API (http://developer.ivona.com/en/speechcloud/actions.html)
-Then, it uploads the speech to Soundcloud and finally answer to the original tweet with the newly created soundcloud media.
-Basically, T2S is an annoying parrot.
+For each new tweet, T2S get the oral speech from the text thanks gTTS.
+Then, it uploads the speech to Soundcloud and finally replies to the original tweet with the newly created soundcloud media.
--- t2s.py
+++ t2s.py
@@ -1,58 +1,70 @@
 #!/usr/bin/python
 from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
-import tweepy
-import pyvona
+from tweepy import OAuthHandler, Stream, API
+from gtts import gTTS
 import json
 import soundcloud
-
+ 
 tw_access_token = ""
 tw_access_token_secret = ""
 tw_consumer_key = ""
 tw_consumer_secret = ""
-iv_access_key="";
-iv_access_secret="";
-sc_client_id="";
-sc_client_secret="";
-sc_access_token="";
-tw_target_id = ""
-tw_target_name = ""
-
+sc_client_id = ""
+sc_client_secret = ""
+sc_username = ""
+sc_password = ""
+target_id = ""
+audio_dir = ""
+file_prefix = ""
+ 
 class StdOutListener(StreamListener):
-	def __init__(self, voice, sc, api):
-		self.voice = voice
-		self.sc = sc
-		self.api = api
-		self.count = 0
+    def __init__(self, api, sc):
+        self.api = api
+        self.sc = sc
+        self.count = 0
+ 
+    def on_data(self, data):
+        jsondata = json.loads(data)
 
-	def on_data(self, data):
-		jsondata = json.loads(data)
-		print jsondata['text']
-		if(jsondata['user']['screen_name'] == tw_target_name):
-			self.count += 1	
-			self.voice.fetch_voice(jsondata['text'].split(' ', 1)[1], 'audio/speech' + str(self.count) + '.mp3')
-			track = self.sc.post('/tracks', track={
-				'title': 'speech'+str(self.count),
-				'asset_data': open('audio/speech'+str(self.count)+'.mp3', 'rb')
-			})
-			print track.permalink_url
-			api.update_status('@' + jsondata['user']['screen_name'] + ' ' + track.permalink_url, jsondata['id_str'])
+        if 'text' in jsondata and 'retweeted_status' not in jsondata and ('in_reply_to_status_id' not in jsondata or not jsondata['in_reply_to_status_id']):
+            text = jsondata['text']
+            if 'display_text_range' in jsondata:
+                range = jsondata['display_text_range']
+                text = text[range[0]:range[1]]
+ 
+            if len(text) > 0:
+                print(text)
+                self.count += 1
+                tts = gTTS(text, lang='fr')
+                filepath = audio_dir + file_prefix + '.mp3'
+                with open(filepath, 'wb') as f:
+                    tts.write_to_fp(f)
+                track = self.sc.post('/tracks', track={
+                    'title': file_prefix + str(self.count),
+                    'asset_data': open(filepath, 'rb')
+                })
+                print(track.permalink_url)
+                api.update_status('@' + jsondata['user']['screen_name'] + ' ' + track.permalink_url, jsondata['id_str'])
+                
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        if status == 420:
+            #returning False in on_data disconnects the stream
+            return False
 
-		return True
-
-	def on_error(self, status):
-		print status
+sc  = soundcloud.Client(
+    client_id=sc_client_id,
+    client_secret=sc_client_secret,
+    username=sc_username,
+    password=sc_password
+)
 
 auth = OAuthHandler(tw_consumer_key, tw_consumer_secret)
 auth.set_access_token(tw_access_token, tw_access_token_secret)
-api = tweepy.API(auth)
-v = pyvona.create_voice(iv_access_key, iv_access_secret)
-v.codec = 'mp3'
-v.voice_name = 'Celine'
-v.speech_rate ='slow'
-sc = soundcloud.Client(access_token=sc_access_token)
-l = StdOutListener(v, sc, api)
-stream = Stream(auth, l)
-
-stream.filter(follow=[tw_target_id])
+api = API(auth)
+listener = StdOutListener(api, sc)
+stream = Stream(auth, listener)
+stream.filter(follow=[target_id])
+ 
